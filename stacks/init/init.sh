e4d09c761ba4e70ce0e892f83f99966985df3308
Felix Chapman <aelred717@gmail.com>
2023-10-25 23:51:02 +0100

    Add initial docker swarm setup

---
+++ stacks/init/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+export DOCKER_HOST="ssh://root@drone.ael.red"
+
+# Check can connect to SSH
+ssh -q "root@drone.ael.red" exit
+
+if [ "$(docker info --format '{{.Swarm.LocalNodeState}}')" == "inactive" ]; then
+    docker swarm init
+fi
+
+docker network create --driver=overlay traefik-public
