8e013c983b8e4dbda584b8c8575cc992180d2940
Felix Chapman <aelred717@gmail.com>
2023-10-30 21:19:24 +0000

    Update providers.tf

--- modules/github_pages/providers.tf
+++ modules/github_pages/providers.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.28.0"
+      source  = "hashicorp/aws"
+      version = "~> 3.30.0"
     }
     github = {
-      source = "integrations/github"
+      source  = "integrations/github"
       version = "~> 4.4.0"
     }
   }
