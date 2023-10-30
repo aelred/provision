09315a9b59c54b388f4ccc19cb7e902f17b38543
Felix Chapman <aelred717@gmail.com>
2023-10-30 21:13:36 +0000

    Add volume to kube

--- prod/kube.tf
+++ prod/kube.tf
@@ -2,8 +2,24 @@ resource "hcloud_server" "kube" {
   name        = "kube"
   server_type = "cpx11"
   image       = "ubuntu-22.04"
-  user_data = templatefile("kube/cloud-init.yml", { fully_qualified_domain_name = "kube.${var.domain}" })
+  user_data = templatefile("kube/cloud-init.yml", {
+    fully_qualified_domain_name = "kube.${var.domain}"
+    linux_device                = hcloud_volume.kube.linux_device
+    mount_dir_name              = "longhorn"
+  })
   ssh_keys = [hcloud_ssh_key.key.id]
+}
+
+resource "hcloud_volume" "kube" {
+  name   = "kube"
+  size   = 10
+  format = "ext4"
+}
+
+resource "hcloud_volume_attachment" "kube" {
+  volume_id = hcloud_volume.kube.id
+  server_id = hcloud_server.kube.id
+  automount = false
 }
 
 resource "aws_route53_record" "wildcard" {
